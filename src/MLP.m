112c9b8203979cb3120f82b05eca94382db36198
Skadoosh777 <pwang2@laurentian.ca>
2023-03-10 23:36:59 -0500

    dataset read

--- src/MLP.m
+++ src/MLP.m
@@ -1,3 +1,23 @@
 clc;
 close all;
 clear;
+
+tic;
+
+file_1 = "Dataset_with_6 inputs and 2 Outputs.xlsx";
+file_2 = "Dataset_5000.xlsx";
+file_3 = "Dataset_300000.xlsx";
+
+file = file_2;
+if (file==file_1)
+    dataset = readmatrix(file_1);
+elseif(file == file_2)
+    dataset = readmatrix(file_2);
+elseif(file == file_3)
+    dataset = readmatrix(file_3);
+end
+
+[R,C] = size(dataset);
+fprintf("R:%d\n",R);
+
+toc;
