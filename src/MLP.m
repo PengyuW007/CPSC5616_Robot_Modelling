d744b64951a2a95b04ed5ce311fa9ea370c058f5
Skadoosh777 <pwang2@laurentian.ca>
2023-03-13 18:19:07 -0400

    Structure built

--- src/MLP.m
+++ src/MLP.m
@@ -18,20 +18,24 @@ elseif(file == file_3)
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%dataset%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%% Initialization %%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 [R,C] = size(dataset);
 fprintf("R:%d\n",R);
 
+TRAIN = 1:0.7*R;
+VALIDATION = (0.7*R+1):0.85*R;
+TEST = (0.85*R+1):R;
+
 L = 6; % 6 inputs
 N = 2; % 2 outputs
-
-% neurons
-M = L-1; % Range = 1 to L, Best = 2/3*L+N or L-1
+M = L-1; % % neurons, Range = 1 to L, Best = 2/3*L+N or L-1
 
 BIAS = 1;
+ETA = 0.25; % 0.1<ETA<0.4
+
 
 low1 = -1/sqrt(L);
 up1 = 1/sqrt(L);
@@ -43,32 +47,89 @@ up2 = 1/sqrt(M);
 w2_1d = low2 + (up2-low2).*rand((M+1)*N,1);
 w2 = reshape(w2_1d,[(M+1),N]);
 
-
-x = zeros(1,L);
-for i = 1:L
-    x(i) = dataset(1,i); % input value, x
-end
-
-Y_ = zeros(1,N);
-for i = 1:N
-    Y_(i) = dataset(1,L+1+i); % Output value, Y hat
-end
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%% Feed- Forward %%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% NET and ACTIVATION (weighted sum) for each neuron I, L - input, M - Neurons
-net1 = zeros(1,M); % Net
-y1 = zeros(1,M); % Activation function
-netCurr = 0;
-for m = 1:M
-    for l = 1:L
-        netCurr = netCurr+x(l)*w1(l,m);
+for r = TRAIN
+    x = zeros(1,L);
+    for i = 1:L
+        x(i) = dataset(r,i); % input value, x
     end
-    net1(m) = netCurr + BIAS*w1(L+1,m);
-    y1(m) = 1/(1+exp(-net1(m)));
-end
 
+    Y_ = zeros(1,N);
+    for i = 1:N
+        Y_(i) = dataset(r,L+1+i); % Output value, Y hat
+    end
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %%%%%%%%%% Feed- Forward %%%%%%%%%%%
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+    %%%%%%%%%%%%%%%%%%
+    % Input - Hidden %
+    %%%%%%%%%%%%%%%%%%
+    % NET and ACTIVATION (weighted sum) for each neuron I, L - input, M - Neurons
+    net1 = zeros(1,M); % Net
+    y1 = zeros(1,M); % Activation function
+    netCurr = 0;
+    for m = 1:M
+        for l = 1:L
+            netCurr = netCurr+x(l)*w1(l,m);
+        end
+        net1(m) = netCurr + BIAS*w1(L+1,m);
+        y1(m) = 1/(1+exp(-net1(m)));
+    end
+
+    %%%%%%%%%%%%%%%%%%%
+    % Hidden - Output %
+    %%%%%%%%%%%%%%%%%%%
+    % NET and ACTIVATION (weighted sum) for each neuron I, M - Neurons, N - Output
+    net2 = zeros(1,N); % Net
+    y2 = zeros(1,N); % Activation function
+    netCurr = 0;
+    for n = 1:N
+        for m = 1:M
+            netCurr = netCurr+y1(m)*w2(m,n);
+        end
+        net2(n) = netCurr + BIAS*w2(M+1,n);
+        y2(n) = net2(n);
+    end
+
+    E = 0.5*(Y_ - y2).^2; % Error cost function
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %%%%%%%%%% Feed- Backward %%%%%%%%%%
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Deltas %
+    % Output unit
+    delta_out = y2.*(1-y2).*(Y_ - y2);
+
+    % Hidden unit
+    delta_hid = zeros(M,1);
+    for n = 1:N
+        for m = 1:M
+            delta_hid(m) = y1(m)*(1 - y1(m))*w2(m,n)*delta_out(n);
+        end
+    end
+
+    % Updated weights %
+    % Output - Hidden
+    %w2_new = zeros(M+1,N);
+    for n = 1:N
+        for m = 1:M
+            w2(m,n) = ETA*delta_out(n)*y1(m) + w2(m,n);
+        end
+        w2(M+1,n) = ETA*delta_out(n)*BIAS + w2(M+1,n);
+    end
+
+    % Hidden - Input
+    %w1_new = zeros(L+1,M);
+    for m = 1:M
+        for l = 1:L
+            w1(l,m) = ETA*delta_hid(m)*x(l) + w1(l,m);
+        end
+        w1(L+1,m) = ETA*delta_hid(m)*BIAS + w1(L+1,m);
+    end
+end % end Training
 toc;
