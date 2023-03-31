830765939cf9c783ceb7a4c532195c58abd45823
Skadoosh777 <pwang2@laurentian.ca>
2023-03-31 14:20:47 -0400

    feed-forward

--- src/MLP.m
+++ src/MLP.m
@@ -23,6 +23,7 @@ end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 [R,C] = size(dataset);
+pre_dataset = dataset;
 index = randperm(R);
 dataset = dataset(index, :);
 
@@ -30,26 +31,84 @@ TRAIN = 1:(0.6*R);
 VALIDATION = (0.6*R+1):0.8*R;
 TEST = (0.8*R+1):R;
 
-L= 6; % 6 inputs
-N = 2; % 2 outputs
-M = L-1; % % neurons, Range = 1 to L, Best = 2/3*L+N or L-1
+IN= 6; % 6 inputs
+OUT = 2; % 2 outputs
+NEURONS = IN-1; % % neurons, Range = 1 to L, Best = 2/3*L+N or L-1
 
 BIAS = 1;
 ETA = 0.25; % 0.1<ETA<0.4
 
-[w1,w1_1d] = weights(L,M);
-[w2,w2_1d] = weights(M,N);
+[w1,w1_1d] = weights(IN,NEURONS);
+[w2,w2_1d] = weights(NEURONS,OUT);
 
-w1_epoch = zeros(R,(L+1)*M);
-w2_epoch = zeros(R,(M+1)*N);
+w1_epoch = zeros(R,(IN+1)*NEURONS);
+w2_epoch = zeros(R,(NEURONS+1)*OUT);
 
-y2_epoch = zeros(R,N);
+y2_epoch = zeros(R,OUT);
 
 Y_epoch = pre_dataset(:,C-1:C);
 
 Error = zeros(R,2);
 
+bp(dataset,IN,NEURONS,OUT,BIAS,ETA,w1,w2);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%% TRAINING %%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 toc;
+
+function bp(dataset,row,L,M,N,bias,eta,w1,w2)
+for r = row
+    x = dataset(r,1:L); % input value, x
+
+    Y_ = dataset(r,C-1:C); % Output value, Y hat
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %%%%%%%%%% Feed- Forward %%%%%%%%%%%
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+    %%%%%%%%%%%%%%%%%%
+    % Input - Hidden %
+    %%%%%%%%%%%%%%%%%%
+    % NET and ACTIVATION (weighted sum) for each neuron I, L - input, M - Neurons
+    net1 = zeros(1,M); % Net
+    y1 = zeros(1,M); % Activation function
+    netCurr = 0;
+    for m = 1:M
+        for l = 1:L
+            netCurr = netCurr+x(l)*w1(l,m);
+        end
+        net1(m) = netCurr + bias*w1(L+1,m);
+        y1(m) = 1/(1+exp(-net1(m))); % Sigmoid function
+    end
+
+    %%%%%%%%%%%%%%%%%%%
+    % Hidden - Output %
+    %%%%%%%%%%%%%%%%%%%
+    % NET and ACTIVATION (weighted sum) for each neuron I, M - Neurons, N - Output
+    net2 = zeros(1,N); % Net
+    y2 = zeros(1,N); % Activation function
+    netCurr = 0;
+    for n = 1:N
+        for m = 1:M
+            netCurr = netCurr+y1(m)*w2(m,n);
+        end
+        net2(n) = netCurr + BIAS*w2(M+1,n);
+        y2(n) = 1/(1+exp(-net2(n))); % Sigmoid function
+    end
+    y2_epoch(r,:) = y2;
+
+    E = (0.5/(0.7*R))*(Y_ - y2).^2; % Error cost function
+    Error(r,:) = E;
+
+end
+
+end % end BP
+
+
 
 function [w,w_1d] = weights(layer1,layer2)
     low = -1/sqrt(layer1);
