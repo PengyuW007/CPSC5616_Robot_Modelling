d2eb0a30a5af81470b4c3831abc4f9f4d2a87546
Skadoosh777 <pwang2@laurentian.ca>
2023-04-03 23:09:10 -0400

    weights update

--- src/LSTM.m
+++ src/LSTM.m
@@ -39,10 +39,49 @@ TEST = (0.8*R+1):R;
 IN= 6; % 6 inputs
 OUT = 2; % 2 outputs
 
+Y_epoch = pre_dataset(:,C-1:C);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%% TRAINING %%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+neurons = IN-1; % % neurons, Range = 1 to L, Best = 2/3*L+N or L-1
+
+BIAS = 1;
+ETA = 0.2; % 0.1<ETA<0.4
+
+lstm(dataset,TRAIN,IN,neurons,OUT,BIAS,ETA);
+
 toc;
 
-function [c,d] = add(a,b)
-c = a;
-d = b;
+function lstm(dataset,row,L,M,N,bias,eta)
+[wForget,wForget_1d] = weights(L);
+h = zeros(1,L);
+
+for r = row
+    x = dataset(r,1:L); % input value, x
+
+    Y_ = dataset(r,L+2:L+1+N); % Output value, Y hat
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %%%%%%%%%% Feed- Forward %%%%%%%%%%%
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+    %%%%%%%%%%%%%%%%%%
+    % Input - Hidden %
+    %%%%%%%%%%%%%%%%%%
+
+
+
+end
 end
 
+function [w,w_1d] = weights(input)
+state = 5;
+low = -1/sqrt(input);
+up = 1/sqrt(input);
+
+w_1d = low + (up-low).*rand((input+1)*state,1);
+w = reshape(w_1d,[state,input+1]);
+end
