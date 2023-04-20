0524609e34a0ced13821817496f638d97129e7c8
Skadoosh777 <pwang2@laurentian.ca>
2023-04-20 11:18:23 -0400

    folder refactor

---
+++ src/LSTM/LSTM.m
@@ -0,0 +1,221 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% COURSE: CPSC 5616
+% MEMBER: 
+%       Pengyu Wang, 0425157
+%       Haokun Zhang, 0424660
+%       Ziping Zhu, 0422426
+% PROJECT: Robot Modelling by Using LSTM
+%
+% REMARKS: Prediction of robot motion
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+clc;
+close all;
+clear;
+
+tic;
+
+file_1 = "Dataset_with_6 inputs and 2 Outputs.xlsx";
+file_2 = "Dataset_5000.xlsx";
+file_3 = "Dataset_300000.xlsx";
+
+file = file_2;
+if (file==file_1)
+    dataset = readmatrix(file_1);
+elseif(file == file_2)
+    dataset = readmatrix(file_2);
+elseif(file == file_3)
+    dataset = readmatrix(file_3);
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%% Initialization %%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+[R,C] = size(dataset);
+pre_dataset = dataset;
+index = randperm(R);
+dataset = dataset(index, :);
+
+TRAIN = 1:(0.6*R);
+VALIDATION = (0.6*R+1):0.8*R;
+TEST = (0.8*R+1):R;
+
+IN= 6; % 6 inputs
+OUT = 2; % 2 outputs
+
+Y_epoch = pre_dataset(:,C-1:C);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%% TRAINING %%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+neurons = IN-1; % % neurons, Range = 1 to L, Best = 2/3*L+N or L-1
+Eta = 0.0001;
+Bias = 1;
+
+[E,w,y] = lstm(dataset,TRAIN,IN,neurons,OUT,Bias,Eta);
+
+plot(E);
+xlabel("Iteration");
+ylabel("Error cost value")
+
+figure;
+plot(w);
+xlabel("Iteration");
+ylabel("weights value")
+
+figure;
+plot(y);
+xlabel("Iteration");
+ylabel("Output value")
+toc;
+
+function [Error,w_epoch,y_epoch] =lstm(dataset,row,L,M,N,bias,Eta)
+W = Ws(L);
+wub = W_ubs(L);
+wy = Weights(M+1,N);
+
+[~,ro] = size(row);
+h = zeros(ro+1,M); % hidden state, start from h0 = 0
+c = zeros(ro+1,M);
+
+w_epoch = zeros(ro,(M+1)*(L+N));
+y_epoch = zeros(ro,2);
+Error = zeros(ro,2);
+for r = row
+    x = dataset(r,1:L); % input value, x
+    Y_ = dataset(r,L+2:L+1+N); % Output value, Y hat
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %%%%%%%%%% Feed- Forward %%%%%%%%%%%
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    for m = 1:M
+        hCurr = h(r,m);
+        xs = [x hCurr bias];
+
+        % Input Gate 1 %
+        wi = W(1,:);
+        wubi = wub(1,:);
+        w = [wi wubi];
+        neti = sum(w.*xs);
+        i = sigmoid(neti);
+        % Input Gate 2 %
+        wc = W(2,:);
+        wubc = wub(2,:);
+        w = [wc wubc];
+        netc = sum(w.*xs);
+        c_ = tanh(netc);
+
+        % Forget Gate %
+        wf = W(3,:);
+        wubf = wub(3,:);
+        w = [wf wubf];
+        netf = sum(w.*xs);
+        f = sigmoid(netf);
+
+        % Output Gate %
+        wo =W(4,:);
+        wubo = wub(4,:);
+        w = [wo wubo];
+        neto = sum(w.*xs);
+        o = sigmoid(neto);
+
+        % Memory Cell/ Cell State %
+        cNext = i.*c_ + f.*c(r,m);
+        c(r+1,m) = cNext;
+
+        % Hidden Layer/ State %
+        hNext = o*tanh(cNext);
+        h(r+1,m) = hNext;
+    end
+
+    for n=1:N
+        for m = 1:M
+            y = wy(m,n)*h(r+1,m);
+        end
+        y(n) = y+bias*wy(M+1,n);
+    end
+
+    y_epoch(r,:) = y;
+
+    E = (0.5/(ro)).*(Y_ - y).^2; % Error cost function
+    Error(r,:) = E;
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %%%%%%%%%% Feed- Backward %%%%%%%%%%
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    delta_ot = (Y_-y).*(-wy).*(tanh(c(r,:)));
+
+    delta_ct = (Y_-y).*(-wy).*o.*(1-tanh(c(r+1,:)).^2); % gradient of the error to the memory cell
+
+    delta_it = delta_ct.*c_; % input gate
+
+    delta_ft = delta_ct.*c(r,m); % forget gate
+
+    delta_at = delta_ct.*i; % new memory state
+
+    %     delta_ct_1 = delta_ct.*f; % previous cell state
+
+    % NET %
+    delta_at_ = delta_at.*(1-tanh(netc).^2);
+
+    delta_it_ = delta_it.*i.*(1-i);
+
+    delta_ft_ = delta_ft.*f.*(1-f);
+
+    delta_ot_ = delta_ot.*o.*(1-o);
+
+    delta_zt = [delta_at_ delta_it_ delta_ft_ delta_ot_];
+    ws = [W wub];
+    %%%%%%%%% WEIRD %%%%%%%%%
+    wTemp = Weights(M-3,L+N);
+    ws_ = [ws; wTemp];
+    %%%%%%%%% WEIRD %%%%%%%%%
+    delta_w = ws_.*delta_zt;
+    ws_ = ws_ - Eta.*delta_w;
+
+    ws_1d = reshape(ws_,[1,(M+1)*(L+N)]);
+    w_epoch(r,:) = ws_1d;
+end
+end
+
+function y = sigmoid(net)
+y = 1/(1+exp(-net));
+end
+
+function y = tanh(net)
+y = (exp(2*net)-1)/(exp(2*net)+1);
+end
+
+function w = Ws(input)
+State = 4;
+low = -1/sqrt(input);
+up = 1/sqrt(input);
+
+w_1d = low + (up-low).*rand(input*State,1);
+w = reshape(w_1d,[State,input]);
+end
+
+function w = W_ubs(input)
+state = 4;
+low = -1/sqrt(input);
+up = 1/sqrt(input);
+
+w_1d = low + (up-low).*rand(state*2,1);
+w = reshape(w_1d,[state,2]);
+end
+
+function w = Weights(layer1,layer2)
+low = -1/sqrt(layer1);
+up = 1/sqrt(layer1);
+
+w_1d = low + (up-low).*rand((layer1)*(layer2),1);
+w = reshape(w_1d,[layer1,layer2]);
+end
---
+++ src/MLP_Batch_tanh/project_v2.m
@@ -0,0 +1,146 @@
+clc;
+close all;
+clear;
+tic
+
+file = 3;
+
+if file == 1
+    data = readtable('Robot Dataset_with_6 inputs and 2 Outputs.xlsx');
+elseif file == 2
+    data = readtable('Dataset_5000.xlsx');
+elseif file == 3
+    data = readtable('Dataset_300000.xlsx');
+end
+
+data = data{:,:};
+
+%x = 1:0.1:100;
+%w = 1:0.1:100;
+%data = [x',w',sin(x+w)',(x+w)', 2*(x+w)'];
+
+% Split the data into training, validation, and testing sets
+numData = size(data, 1);
+numTrain = round(0.6*numData);
+numVal = round(0.2*numData);
+numTest = numData - numTrain - numVal;
+trainData = data(1:numTrain, :);
+valData = data(numTrain+1:numTrain+numVal, :);
+testData = data(numTrain+numVal+1:end, :);
+
+% Define the MLP architecture
+numInputNeurons = 6; % 6 inputs
+numHiddenNeurons = 6; % Starting with 5 neurons in the hidden layer
+numOutputNeurons = 2;
+
+% Define the learning rate and the number of epochs for training
+learningRate = 0.000001;
+numEpochs = 2000;
+bias = 1;
+
+% Initialize variables to store the best weights and validation error
+bestWeightsInputToHidden = [];
+bestWeightsHiddenToOutput = [];
+bestValidationError = inf;
+%weight1Record = zeros(numTrain*numEpochs,(numInputNeurons+1)*numHiddenNeurons);
+%weight2Record = zeros(numTrain*numEpochs,(numHiddenNeurons+1)*numOutputNeurons);
+valLoss = zeros(numEpochs,1);
+trainLosses = zeros(numEpochs,1);
+
+% Train the MLP with different numbers of neurons in hidden layer
+for numHiddenNeurons = numHiddenNeurons:numHiddenNeurons
+    % Initialize the weights for the MLP
+    weightsInputToHidden = randn(numInputNeurons + 1, numHiddenNeurons);
+    weightsHiddenToOutput = randn(numHiddenNeurons + 1, numOutputNeurons);
+
+    % Train the MLP using backpropagation
+    for epoch = 1:numEpochs
+        % Shuffle the training data
+        shuffledIndices = randperm(numTrain);
+        shuffledData = trainData(shuffledIndices, :);
+
+        % Forward pass through the MLP
+        input = [bias .* ones(numTrain,1), shuffledData(:, 1:numInputNeurons)]; % Add bias term
+        hidden = tanh(input * weightsInputToHidden); % Activation function
+        output = tanh([bias .* ones(numTrain,1), hidden] * weightsHiddenToOutput); % Activation function 
+
+        % Update the weights using backward pass through the MLP
+        %===============================================================================================================================
+        delta = (shuffledData(:, numInputNeurons+2:numInputNeurons+3) - output) .* (1 - output.^2);
+        deltas = (delta * weightsHiddenToOutput(2:end,:)') .* (1 - hidden.^2);
+        
+        weightsHiddenToOutput = weightsHiddenToOutput + [bias .* ones(numTrain,1), hidden]' * (learningRate * delta);
+        weightsInputToHidden = weightsInputToHidden + (input' * (learningRate * deltas));
+        
+        %weight1Record(i+(epoch-1)*numTrain,:) = reshape(weightsInputToHidden.',1,[]);
+        %weight2Record(i+(epoch-1)*numTrain,:) = reshape(weightsHiddenToOutput.',1,[]);
+
+        % Compute the training error
+        t = shuffledData(:, numInputNeurons+2:end) - output;
+        trainLoss = sum(sum((t).^2))/numTrain;
+        trainLosses(epoch,:) = trainLoss;
+
+        % Compute the validation error
+        input = [bias .* ones(numVal, 1), valData(:, 1:numInputNeurons)]; % Add bias term
+        hidden = tanh(input * weightsInputToHidden); % Activation function
+        output = tanh([bias .* ones(numVal,1), hidden] * weightsHiddenToOutput); % Activation function  
+        t = valData(:, numInputNeurons+2:end) - output;
+        validationLoss = sum(sum((t).^2))/numVal;
+        valLoss(epoch,:) = validationLoss;
+        if mod(epoch, 100) == 0
+            fprintf('Epoch %d, Loss: %f\n', epoch, validationLoss);
+        end
+
+        % Store the best weights and validation error
+        if validationLoss < bestValidationError
+            bestWeightsInputToHidden = weightsInputToHidden;
+            bestWeightsHiddenToOutput = weightsHiddenToOutput;
+            bestValidationError = validationLoss;
+        elseif validationLoss > bestValidationError
+            %break;
+        end
+    end
+%{
+    r = 1:numTrain*numEpochs;
+
+
+    figure(1);
+    subplot(2,1,1);
+    plot(r, weight1Record, r, repmat(reshape(bestWeightsInputToHidden.',1,[]), numTrain*numEpochs, 1));
+    title("Weights from Input to Hidden vs Iteration");
+    subplot(2,1,2);
+    plot(r, weight2Record, r, repmat(reshape(bestWeightsHiddenToOutput.',1,[]), numTrain*numEpochs, 1));
+    title("Weights from Hidden to Output vs Iteration");
+%}
+
+    figure(3);
+    plot(1:numEpochs, trainLosses, 'yellow', 1:numEpochs, valLoss, 'blue');
+    title("Train Loss(Yellow) and Validation Loss(Blue) vs Number of Epoch");
+    legend('Train Loss','Validation Loss');
+
+%{
+    % Stop increasing the number of hidden neurons if there is no change in validation error
+    if numHiddenNeurons > 5 && validationError >= prevValidationError
+        break;
+    end
+    prevValidationError = validationError;
+%}
+end
+
+% Evaluate the MLP on the testing set
+input = [bias .* ones(numTest, 1), testData(:, 1:numInputNeurons)]; % Add bias term
+hidden = tanh(input * bestWeightsInputToHidden); % Activation function
+output = tanh([bias .* ones(numVal,1), hidden] * bestWeightsHiddenToOutput); % Activation function  
+
+figure(4);
+subplot(2,1,1);
+plot(1:numTest, testData(:,end-1), 'green', 1:numTest, output(:,1), 'blue')
+subplot(2,1,2);
+plot(1:numTest, testData(:,end), 'green', 1:numTest, output(:,2), 'blue')
+
+%testError = sqrt(mean(sum(testData(:, numInputNeurons+2:end) - output, 2).^2));
+%testError = mean(rms(testData(:, numInputNeurons+2:end) - output, 2));
+
+%fprintf('Test Error: %d\n', testError);
+toc
+
