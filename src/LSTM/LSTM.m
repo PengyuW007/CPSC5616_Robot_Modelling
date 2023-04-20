a5d9fd15232541d768eeed3273c1e342949100bc
Skadoosh777 <pwang2@laurentian.ca>
2023-04-20 11:43:21 -0400

    Update LSTM.m

--- src/LSTM/LSTM.m
+++ src/LSTM/LSTM.m
@@ -21,7 +21,7 @@ file_1 = "Dataset_with_6 inputs and 2 Outputs.xlsx";
 file_2 = "Dataset_5000.xlsx";
 file_3 = "Dataset_300000.xlsx";
 
-file = file_2;
+file = file_1;
 if (file==file_1)
     dataset = readmatrix(file_1);
 elseif(file == file_2)
